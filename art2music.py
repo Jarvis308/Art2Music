608a27ce9df1714a38c8a7cc822f28c21adf4d3f
Jarvis308 <arvesej01@sunyjcc.edu>
2023-05-12 18:12:47 -0400

    Added header image

--- art2music.py
+++ art2music.py
@@ -211,8 +211,8 @@ def img2music(img, scale = [220.00, 246.94 ,261.63, 293.66, 329.63, 349.23, 415.
     return song, pixels_df, harmony
 
 
-
-st.header("") 
+banner = "banner.jpeg"
+st.header.image(banner) 
 hide_st_style = """
             <style>
             footer {visibility: hidden;}
