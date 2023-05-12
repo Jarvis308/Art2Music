93c33daf54894419c04093101f1f22f524bb198e
Jarvis308 <arvesej01@sunyjcc.edu>
2023-05-12 18:27:20 -0400

    Update

--- art2music.py
+++ art2music.py
@@ -211,8 +211,7 @@ def img2music(img, scale = [220.00, 246.94 ,261.63, 293.66, 329.63, 349.23, 415.
     return song, pixels_df, harmony
 
 
-banner = Image.open('banner.jpeg')
-st.header.image(banner, channels = "RGB", output_format="auto") 
+st.header("")
 hide_st_style = """
             <style>
             footer {visibility: hidden;}
