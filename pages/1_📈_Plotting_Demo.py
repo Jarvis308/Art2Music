931f09f059e3b1e42bccb6919e0f4c314c7da250
Jarvis308 <arvesej01@sunyjcc.edu>
2023-05-12 18:32:38 -0400

    Update

--- pages/1_📈_Plotting_Demo.py
+++ pages/1_📈_Plotting_Demo.py
@@ -11,6 +11,7 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.markdown("# Plotting Demo")
 st.sidebar.header("Plotting Demo")
